9dbc3a8206ffc23b2a89562eb83b546dca1942d2
TheProgxy <theprogxy@gmail.com>
2024-06-03 16:44:53 +0200

    Fix

--- app/src/main/java/com/theprogxy/tamtam/ConfirmEmail.kt
+++ app/src/main/java/com/theprogxy/tamtam/ConfirmEmail.kt
@@ -15,8 +15,18 @@ class ConfirmEmail : Activity() {
 
         val email = intent.getStringExtra("email")
         confirmText = findViewById(R.id.confirmText)
-        val confirmationText = "Confirm the account by visiting the link sent to $email"
-        confirmText.text = confirmationText
+        val confirmationMessage = """
+            Account Created Successfully!
+            
+            A confirmation link has been sent to $email. Please check your email and click on the link to confirm your account.
+
+            If you do not see the email in your inbox, please check your spam or junk folder.
+            
+            Thank you for registering with us!
+            
+            The TamTam team.
+        """.trimIndent()
+        confirmText.text = confirmationMessage
 
         nextButton = findViewById(R.id.nextButton)
         nextButton.setOnClickListener {
