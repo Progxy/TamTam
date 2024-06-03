7918efd030daf42eb16ca9bf98a52721527a60fe
TheProgxy <theprogxy@gmail.com>
2024-06-03 14:45:31 +0200

    First Commit

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -45,7 +45,7 @@ dependencies {
     implementation(libs.osmdroid.wms)
     implementation(libs.androidx.appcompat)
     implementation(libs.androidx.coordinatorlayout)
-    implementation(libs.firebase.bom)
+    implementation(platform(libs.firebase.bom))
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
