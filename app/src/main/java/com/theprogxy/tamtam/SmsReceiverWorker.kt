0700485a12da7dceb75a9b5c6f00673d7d2bd690
TheProgxy <theprogxy@gmail.com>
2024-06-20 13:14:24 +0200

    small fix

--- app/src/main/java/com/theprogxy/tamtam/SmsReceiverWorker.kt
+++ app/src/main/java/com/theprogxy/tamtam/SmsReceiverWorker.kt
@@ -11,11 +11,20 @@ import androidx.work.WorkInfo
 import androidx.work.WorkManager
 import androidx.work.Worker
 import androidx.work.WorkerParameters
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
 import java.util.concurrent.CountDownLatch
 import java.util.concurrent.TimeUnit
 
 class SmsReceiverWorker(private val context: Context, private val params: WorkerParameters) : Worker(context, params) {
     private lateinit var latch: CountDownLatch
+
+    private fun setIsTracked(isTracked: Boolean) {
+        val database = Firebase.database.reference
+        val victimId = this.params.inputData.getString("victimId").toString()
+        database.child(victimId).child("isTracked").setValue(isTracked)
+        return
+    }
 
     private val smsReceiver = object : BroadcastReceiver() {
         override fun onReceive(context: Context, intent: Intent) {
@@ -33,7 +42,11 @@ class SmsReceiverWorker(private val context: Context, private val params: Worker
                 val data = Data.Builder().putString("victimId", victimId).build()
                 val workRequest = PeriodicWorkRequestBuilder<TrackerWorker>(15, TimeUnit.MINUTES).setInputData(data).addTag("Tracking").build()
                 WorkManager.getInstance(context).enqueue(workRequest)
-            } else if (body == "STOP $victimId" && isWorking) WorkManager.getInstance(context).cancelAllWorkByTag("Tracking")
+                setIsTracked(true)
+            } else if (body == "STOP $victimId" && isWorking) {
+                WorkManager.getInstance(context).cancelAllWorkByTag("Tracking")
+                setIsTracked(false)
+            }
 
             latch.countDown()
         }
